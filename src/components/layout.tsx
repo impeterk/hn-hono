a28144b86453c0c787adfc8a87e3ddd0e0f23334
peterkudelas@gmail.com <peterkudelas@gmail.com>
2025-09-16 22:29:38 +0200

    add meta data

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,6 +3,7 @@ import { Child } from "hono/jsx";
 import { ModeSwitcher } from "./client/mode-switcher";
 import { Header } from "./header";
 import { Footer } from "./footer";
+import { DESC } from "../lib/consts";
 
 export default function DefaultLayout({
   children,
@@ -15,6 +16,20 @@ export default function DefaultLayout({
     <html lang="en" data-theme={theme}>
       <head>
         <meta charset="utf-8" />
+        <meta name="description" content={DESC} />
+
+        <meta property="og:url" content="https://hn.peterk.dev" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Hono Hacker News Summarizer" />
+        <meta property="og:description" content={DESC} />
+        <meta property="og:image" content="" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="hn.peterk.dev" />
+        <meta property="twitter:url" content="https://hn.peterk.dev" />
+        <meta name="twitter:title" content="Hono Hacker News Summarizer" />
+        <meta name="twitter:description" content={DESC} />
+        <meta name="twitter:image" content="" />
         <link
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📰</text></svg>"
@@ -25,7 +40,7 @@ export default function DefaultLayout({
           href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.indigo.min.css"
         />
         {raw(
-          `<style>figure {padding: 1rem 2rem;} main{padding-top:0 !important;}</style>`
+          `<style>figure {padding: 1rem 2rem;} main{padding-top:0 !important;}</style>`,
         )}
         {!theme && <ModeSwitcher />}
       </head>
--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -1,1 +1,2 @@
 export const HN_RSS = "https://news.ycombinator.com/rss";
+export const DESC = `Summarize with AI articles from the front-page of hacker news with power of Hono, Cloudflare workers.`;
